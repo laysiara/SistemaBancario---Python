a12a93fb66e11c5f9c3c36523bfdb0935afe0848
laysiara <laysiara569@gmail.com>
2025-09-29 15:31:51 -0300

    Login e cadastros feitos

--- corpo.py
+++ corpo.py
@@ -1,17 +1,37 @@
 import inquirer
 import funcoes
 
-perguntas = [
-    inquirer.List(
-        'escolha',
-        message= "Faça o login (se não, faça o cadastro)",
-        choices=['Sacar', 'Depositar', 'Ver extrato', 'Transferencia']
-    )
-]
+contas_bancarias=[['iaralays','11407','123'],['ana','78654','567']]
+saldos=[3000,10000]
 
-resposta = inquirer.prompt(perguntas)
+continuaçao=True
+while continuaçao:
+    
+    entrar= [
+        inquirer.List(
+            'entrada',
+            message= "Banco Nacional",
+            choices=['Login', 'Cadastro','Sair']
+        )
+    ]
 
+    forma=inquirer.prompt(entrar)
 
-if resposta == "Sacar":
-    sacar=1 #teste,ignorem essa linha
-    #teste
+    if forma['entrada']== "Login":
+
+        
+        indice=funcoes.Login(contas_bancarias)
+
+        funcoes.Menu(indice,contas_bancarias)
+        
+    elif forma['entrada']=="Cadastro":
+
+        indice=funcoes.Cadastrar(contas_bancarias,saldos)
+        funcoes.Menu(indice,contas_bancarias)
+        
+        
+    elif forma['entrada']=="Sair":
+        continuaçao=False
+        
+    print(contas_bancarias,saldos)
+    
--- funcoes.py
+++ funcoes.py
@@ -1,8 +1,41 @@
+import inquirer
 
-def Cadastrar(nome,senha):
-    return 0
-def Login(nome,senha):
-    return 0
+def Cadastrar(contas_bancarias,saldos):
+    entrar=[inquirer.Text('nome', message="Digite seu nome"),
+           inquirer.Text('cpf',message="Digite seu cpf"),inquirer.Password('senha', message= "Digite sua senha")]
+    
+    saldos.append(0)
+    
+    
+    
+    cadastro=inquirer.prompt(entrar)
+    cadastro=list(cadastro.values())
+    contas_bancarias.append(cadastro)
+
+    indice=contas_bancarias.index(cadastro)
+
+    return indice
+    
+
+def Login(contas_bancarias):
+    entrar=[inquirer.Text('nome', message="Digite seu nome"),
+           inquirer.Text('cpf',message="Digite seu cpf"),inquirer.Password('senha', message= "Digite sua senha")]
+    
+    login=inquirer.prompt(entrar)
+    login=list(login.values())
+    
+    while login not in contas_bancarias:
+        print("Usuário não encontrado, ou senha incorreta")
+        entrar=[inquirer.Text('nome', message="Digite seu nome"),
+           inquirer.Text('cpf',message="Digite seu cpf"),inquirer.Password('senha', message= "Digite sua senha")]
+    
+        login=inquirer.prompt(entrar)
+        login=list(login.values())
+
+    indice=contas_bancarias.index(login)
+
+    return indice
+
 
 def Sacar(saldo):
     return 0
@@ -18,3 +51,26 @@ def Transferencia(saldo):
 
 def Consultar_saldo(saldo):
     return 0
+
+def Menu (indice,contas_bancarias):
+    
+        continuar=True
+        while continuar:
+
+            perguntas = [
+                inquirer.List(
+                    'escolha',
+                    message= (f"Bem vindo(a) {contas_bancarias[indice][0]}. O que deseja realizar?"),
+                    choices=['Sacar', 'Depositar', 'Ver extrato', 'Transferencia',"Sair"]
+                )
+            ]
+
+            resposta = inquirer.prompt(perguntas)
+
+
+            if resposta['escolha'] == "Sacar":
+                sacar=1 #teste,ignorem essa linha
+                #teste
+
+            elif resposta['escolha']== "Sair":
+                continuar=False
